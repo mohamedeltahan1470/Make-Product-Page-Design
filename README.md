bd2d67d5c455ae62044d6b64c26a8bca3b641fe5
Mohammed Ali <158100369+mohamedeltahan1470@users.noreply.github.com>
2024-10-08 09:29:15 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Product Page Design
+
+This project showcases a responsive product page for an online clothing store featuring a casual T-shirt. It includes a product image gallery, size and color selection, quantity input, and a purchase button.
+
+## Features
+
+- Responsive design that adjusts to various screen sizes.
+- Image gallery with thumbnail controls.
+- Size and color selection using radio buttons.
+- Quantity input to specify the number of items.
+- "Buy Now" button for purchasing the product.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Project Structure
+
--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-let procuctImg = document.getElementById("productImg");
+
+
+let productImg = document.getElementById("productImg");
 let btn = document.getElementsByClassName("btn");
 
-btn[0].onclick = function(){
-    procuctImg.src = "../img/image1.png";
-    for(btns of btn){
-        btns.classList.remove("active");
+btn[0].onclick = function () {
+    productImg.src = "./img/image1.png";
+    updateActiveButton(this);
+};
+
+btn[1].onclick = function () {
+    productImg.src = "./img/image2.png";
+    updateActiveButton(this);
+};
+
+btn[2].onclick = function () {
+    productImg.src = "./img/image3.png";
+    updateActiveButton(this);
+};
+
+function updateActiveButton(activeBtn) {
+    for (let button of btn) {
+        button.classList.remove("active");
     }
-    this.classList.add("active");
-}
-btn[1].onclick = function(){
-    procuctImg.src = "../img/image2.png";
-    for(btns of btn){
-        btns.classList.remove("active");
-    }
-    this.classList.add("active");
-}
-btn[2].onclick = function(){
-    procuctImg.src = "../img/image3.png";
-    for(btns of btn){
-        btns.classList.remove("active");
-    }
-    this.classList.add("active");
+    activeBtn.classList.add("active");
 }
